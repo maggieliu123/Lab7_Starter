d73d4d897e212164b0e6a81fddc3add2cf9ddb26
Maggie Liu <maggieliu.252@gmail.com>
2022-11-14 23:22:35 -0800

    finished explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -44,16 +44,37 @@ function initializeServiceWorker() {
   /*******************/
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
-  // B1. TODO: Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO: Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO: Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO: Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO: In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  const registerServiceWorker = async () => {
+    // B1. TODO: Check if 'serviceWorker' is supported in the current browser
+    if ('serviceWorker' in navigator) {
+      // B2. TODO: Listen for the 'load' event on the window object.
+      window.addEventListener ('load', async (event) => { 
+        // Steps B3-B6 will be *inside* the event listener's function created in B2
+        try {
+          // B3. TODO: Register './sw.js' as a service worker (The MDN article
+          //            "Using Service Workers" will help you here)
+          const registration = await navigator.serviceWorker.register("/sw.js", {
+            scope: "./",
+          });
+          // B4. TODO: Once the service worker has been successfully registered, console
+          //            log that it was successful.
+          if (registration.active) {
+            console.log("Registration successful!");
+          }
+        }
+        catch (error) {
+          // B5. TODO: In the event that the service worker registration fails, console
+          //            log that it has failed.
+          console.error("Registration failed with ${error}");
+          // STEPS B6 ONWARDS WILL BE IN /sw.js
+        }
+      });
+    
+    }
+  };
+
+  registerServiceWorker();
+  
 }
 
 /**
@@ -121,8 +142,6 @@ async function getRecipes() {
         console.error(error);
         // A11. TODO: Pass any errors to the Promise's reject() function
         reject(error);
-        // expected output: ReferenceError: nonExistentFunction is not defined
-        // Note - error messages will vary depending on browser
       }
 
     }
